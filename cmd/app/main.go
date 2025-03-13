de6590d57ddc4f6ebdd13ef0642fc8c10460c1c6
IlyaChgn <chugin.ilya2004@gmail.com>
2025-03-14 00:23:06 +0300

    added mode flag

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
 	"github.com/joho/godotenv"
 	"log"
 
 	app "github.com/IlyaChgn/2025_IMAO_DnD_Assistant_backend/internal/pkg/server"
 )
 
+var productionFlag = flag.Bool("prod", false, "Run in production mode")
+
 func main() {
-	err := godotenv.Load(".env")
+	var err error
+
+	flag.Parse()
+	if *productionFlag {
+		log.Println("Running in production mode")
+		err = godotenv.Load("prod.env")
+	} else {
+		log.Println("Running in development mode")
+		err = godotenv.Load(".env")
+	}
+
 	if err != nil {
 		log.Fatal("Error loading env file ", err)
 	}
